de6c6a7ac0cb82badb0c3b2b033b2e3ccd0f260f
unlitgunk02 <120967495+unlitgunk02@users.noreply.github.com>
2025-11-09 07:20:45 -0600

    Change prompt from 'cyberpunk cat' to 'tech-savvy sloth'

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "cyberpunk cat",
+      prompt: "tech-savy sloth",
     };
 
     const response = await env.AI.run(
